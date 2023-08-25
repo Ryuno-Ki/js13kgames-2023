c30a4434b0a12dd9c237fb481bd0bc5c7821f97e
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-25 23:25:00 +0200

    chore: add JSDoc comments to el.js
    This is a neat little helper function that helps me trim down on the
    file size by using a data structure instead of strings.
    Turns out that innerHTML with template literals can take up quite some
    size.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/el.js
+++ src/js/components/el.js
@@ -1,16 +1,34 @@
+/**
+ * Helper function to generate DOM elements from a data structure.
+ *
+ * The last argument is basically arguments of this function.
+ *
+ * @argument {string} name
+ * @argument {Array<string>} classList
+ * @argument {Object<string, *>} attributes
+ * @argument {string} text
+ * @argument {Array<*>} children
+ * @returns {HTMLElement}
+ */
 export function el (name, classList = [], attributes = {}, text = '', children = []) {
   const element = document.createElement(name)
+
   classList.forEach(function addClassName (className) {
     element.classList.add(className)
   })
+
   Object.entries(attributes).forEach(function setAttribute (attribute) {
     const key = attribute[0]
     const value = attribute[1]
     element.setAttribute(key, value)
   })
+
   element.textContent = text
   children.forEach(function recurse (child) {
+    // I don't know how to properly define the type here
+    // @ts-ignore
     element.appendChild(el(...child))
   })
+
   return element
 }
--- types/components/el.d.ts
+++ types/components/el.d.ts
@@ -1,1 +1,14 @@
-export function el(name: any, classList?: any[], attributes?: {}, text?: string, children?: any[]): any;
+/**
+ * Helper function to generate DOM elements from a data structure.
+ *
+ * The last argument is basically arguments of this function.
+ *
+ * @argument {string} name
+ * @argument {Array<string>} classList
+ * @argument {Object<string, *>} attributes
+ * @argument {string} text
+ * @argument {Array<*>} children
+ */
+export function el(name: string, classList?: Array<string>, attributes?: {
+    [x: string]: any;
+}, text?: string, children?: Array<any>): HTMLElement;
