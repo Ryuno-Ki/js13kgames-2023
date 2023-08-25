fc50a9852d4ca1745f1b3dfbf544f60d1d92344f
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-25 22:45:03 +0200

    chore: add JSDoc comments to cityOverview
    This is now type checked.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/city-overview.js
+++ src/js/components/city-overview.js
@@ -1,8 +1,20 @@
 import { el } from './el.js'
 
+/**
+ * Component to display the overview on a city.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../state/initial-state.js').State} state
+ */
 export function cityOverview (targetElement, state) {
-  const element = targetElement.cloneNode(true)
+  const element = /** @type {HTMLElement} */(targetElement.cloneNode(true))
+
   const city = state.cities.find((city) => city.name === state.activeCity)
+  if (!city) {
+    console.warn('Could not find city')
+    return element
+  }
+
   const destinations = state.cities.filter((city) => city.name !== state.activeCity)
   const ships = state.ships
     .filter((ship) => ship.position === state.activeCity)
@@ -16,5 +28,6 @@ export function cityOverview (targetElement, state) {
     ['div', [], { 'data-component': 'market', 'data-type': 'Demand', 'data-wares': JSON.stringify(city.demand) }],
     ['button', [], { type: 'button', 'data-view': 'sea' }, 'See other city']
   ]))
+
   return element
 }
---
+++ types/components/city-overview.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Component to display the overview on a city.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../state/initial-state.js').State} state
+ */
+export function cityOverview(targetElement: HTMLElement, state: import('../state/initial-state.js').State): HTMLElement;
