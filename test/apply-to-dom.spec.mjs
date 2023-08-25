3653063d842dc32083498fcde5dffcc24218e0e4
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-25 15:55:43 +0200

    test: write my first test
    I was surprised to notice that I didn't wrote tests for my previous
    entries. That's so not me.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/apply-to-dom.spec.mjs
@@ -0,0 +1,135 @@
+import chai from 'chai'
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import { applyToDOM } from '../src/js/apply-to-dom.js'
+
+chai.use(sinonChai)
+const { expect } = chai
+
+describe('applyToDOM', () => {
+	it('should remove a DOM node if it is not present in the virtual DOM', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = document.createElement('button')
+		const virtualNode = null
+		sinon.spy(realNode, 'remove')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(realNode.remove).to.have.been.calledOnce
+	})
+
+	it('should add a node to the DOM if it is present in the virtual DOM', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = null
+		const virtualNode = document.createElement('button')
+		sinon.spy(parentNode, 'appendChild')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(parentNode.appendChild).to.have.been.calledOnce
+		expect(parentNode.appendChild).to.have.been.calledWith(virtualNode)
+	})
+
+	it('should replace a real node if attribute values differ', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = document.createElement('button')
+		realNode.type = 'button'
+		const virtualNode = document.createElement('button')
+		virtualNode.type = 'submit'
+		sinon.spy(realNode, 'replaceWith')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(realNode.replaceWith).to.have.been.calledOnce
+		expect(realNode.replaceWith).to.have.been.calledWith(virtualNode)
+	})
+
+	it('should replace a real node if tag names differ', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = document.createElement('button')
+		const virtualNode = document.createElement('a')
+		sinon.spy(realNode, 'replaceWith')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(realNode.replaceWith).to.have.been.calledOnce
+		expect(realNode.replaceWith).to.have.been.calledWith(virtualNode)
+	})
+
+	it('should replace a real node if number of attributes differ', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = document.createElement('button')
+		realNode.type = 'button'
+		const virtualNode = document.createElement('button')
+		sinon.spy(realNode, 'replaceWith')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(realNode.replaceWith).to.have.been.calledOnce
+		expect(realNode.replaceWith).to.have.been.calledWith(virtualNode)
+	})
+
+	it('should replace a real node if text content differ', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = document.createElement('button')
+		realNode.textContent = 'click me'
+		const virtualNode = document.createElement('button')
+		sinon.spy(realNode, 'replaceWith')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(realNode.replaceWith).to.have.been.calledOnce
+		expect(realNode.replaceWith).to.have.been.calledWith(virtualNode)
+	})
+
+	it('should not replace identical nodes', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = document.createElement('button')
+		const virtualNode = document.createElement('button')
+		sinon.spy(realNode, 'replaceWith')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(realNode.replaceWith).not.to.have.been.called
+	})
+
+	it('should recurse into children', () => {
+		// Arrange
+		const parentNode = document.createElement('div')
+		const realNode = document.createElement('button')
+		const icon = document.createElement('i')
+		realNode.appendChild(icon)
+		const virtualNode = document.createElement('button')
+		sinon.spy(realNode, 'replaceWith')
+		sinon.spy(icon, 'remove')
+
+		// Act
+		applyToDOM(parentNode, realNode, virtualNode)
+
+		// Assert
+		expect(realNode.replaceWith).not.to.have.been.called
+		expect(icon.remove).to.have.been.calledOnce
+	})
+})
--- test/require.js
+++ test/require.js
@@ -1,8 +1,13 @@
-global.document = {};
+import { JSDOM } from 'jsdom'
+
+const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>', { url: 'https://jaenis.ch/' })
+
+global.document = dom.window.document
 
 global.window = {
-	localStorage: {
-		getItem: () => null,
-		setItem: () => {},
-	},
-};
+  ...dom.window,
+  localStorage: {
+    getItem: () => null,
+    setItem: () => {}
+  }
+}
