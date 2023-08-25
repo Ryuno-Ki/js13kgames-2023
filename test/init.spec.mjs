16bfe80a9dccf426d1cd8680eafa5d3ccbb75227
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-25 16:23:03 +0200

    test: write tests for init
    This is the entry point to my code. It is called on window.load.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/init.spec.mjs
@@ -0,0 +1,22 @@
+import chai from 'chai'
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import { init } from '../src/js/init.js'
+
+chai.use(sinonChai)
+const { expect } = chai
+
+describe('init', () => {
+	it('should initialise the game', () => {
+		// Arrange
+		sinon.spy(window, 'requestAnimationFrame')
+
+		// Act
+		init()
+
+		// Assert
+		expect(window.requestAnimationFrame).to.have.been.calledOnce
+	})
+})
+	
