5634724532d9bee34e50f5352da10964bb93879f
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-25 23:38:41 +0200

    chore: add JSDoc comments to market component
    This also contains the fix for renaming the warehouse amount to
    quantity.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/market.js
+++ src/js/components/market.js
@@ -1,14 +1,23 @@
 import { el } from './el.js'
 
+/**
+ * Component to display the market in a city.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
 export function market (targetElement, state) {
-  const element = targetElement.cloneNode(true)
-  const type = targetElement.getAttribute('data-type')
-  const wares = JSON.parse(targetElement.dataset.wares)
+  const element = /** @type {HTMLElement} */(targetElement.cloneNode(true))
   element.innerHTML = ''
+
+  const type = /** @type {'demand' | 'supply'} */(element.getAttribute('data-type'))
+  const maybeWares = element.dataset.wares
+  const wares = /** @type {Array<import('../state/initial-state.js').CityDemand | import('../state/initial-state.js').CitySupply>} */(JSON.parse(maybeWares || '[]'))
 
   element.appendChild(el('div', [], {}, type, [
     ['ul', [], {}, '', wares.map((ware) => [
-      'li', [], {}, `${ware.ware}: ${ware.amount}`
+      'li', [], {}, `${ware.ware}: ${ware.quantity}`
     ])]
   ]))
 
---
+++ types/components/market.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Component to display the market in a city.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
+export function market(targetElement: HTMLElement, state: import('../state/initial-state.js').State): HTMLElement;
