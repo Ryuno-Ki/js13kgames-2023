cfe3318ef368c3deaa74c49a8bfe2c85c09716d8
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-28 19:55:01 +0200

    chore: add JSDoc comments to World Selection scene
    This is the „overworld” so to speak. I'm using it for picking a scenario
    here.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/scenes/world-selection.js
+++ src/js/components/scenes/world-selection.js
@@ -1,12 +1,30 @@
 import { el } from '../el.js'
 
+/**
+ * Component to choose between scenarios.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
 export function sectionWorldselection (targetElement, state) {
-  const element = targetElement.cloneNode(true)
+  const element = /** @type {HTMLElement} */(targetElement.cloneNode(true))
   element.innerHTML = ''
+
   if (state.activeScene === 'world-section') {
-    element.appendChild(el('div', [], {}, '', [['h1', [], {}, 'World'], ['div', ['actions'], {}, '', [['button', ['action'], {
-      'data-scene': 'level-section'
-    }, 'Level']]]]))
+    element.appendChild(el('div', [], {}, '', [
+      ['h1', [], {}, 'World'],
+      ['label', [], { for: 'level-scenario' }, 'Choose your scenario'],
+      ['select', [], { id: 'level-scenario' }, '', [
+        ['option', [], { value: '', checked: 'checked' }, 'Please choose'],
+        ['option', [], { value: 'tutorial' }, 'Learn the ropes'],
+        ['option', [], { value: 'free-play' }, 'Sail away']
+      ]],
+      ['div', ['actions'], {}, '', [
+        ['button', ['action'], { 'data-scene': 'level-section' }, 'Level']
+      ]]
+    ]))
   }
+
   return element
 }
--- types/components/scenes/world-selection.d.ts
+++ types/components/scenes/world-selection.d.ts
@@ -1,1 +1,8 @@
-export function sectionWorldselection(targetElement: any, state: any): any;
+/**
+ * Component to choose between scenarios.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
+export function sectionWorldselection(targetElement: HTMLElement, state: import('../../state/initial-state.js').State): HTMLElement;
