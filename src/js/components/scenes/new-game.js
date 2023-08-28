e01d05d8cd01f62d905b03063b48ae510dffb624
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-28 15:14:20 +0200

    feat: accept playername input
    I'm not handling the information right now.
    It must be entered before I allow to move to the next scene. This
    validation logic is not present. I should also strip „dangerous”
    characters that could be interpreted as code or markup before committing
    the value to the store. Therefore I will display the „sanitized”
    information as feedback.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/scenes/new-game.js
+++ src/js/components/scenes/new-game.js
@@ -1,14 +1,27 @@
 import { el } from '../el.js'
 
+/**
+ * Component to query for player information such as display name.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
 export function sectionNewgame (targetElement, state) {
-  const element = targetElement.cloneNode(true)
+  const element = /** @type {HTMLElement} */(targetElement.cloneNode(true))
   element.innerHTML = ''
+
   if (state.activeScene === 'new-game-section') {
-    element.appendChild(el('div', [], {}, '', [['h1', [], {}, 'New game'], ['div', ['actions'], {}, '', [['button', ['action'], {
-      'data-scene': 'title-section'
-    }, 'Title'], ['button', ['action'], {
-      'data-scene': 'world-section'
-    }, 'World']]]]))
+    element.appendChild(el('div', [], {}, '', [
+      ['h1', [], {}, 'New game'],
+      ['label', [], { for: 'playername' }, 'How do you want to be known?'],
+      ['input', [], { id: 'playername', type: 'text', max: '64' }],
+      ['div', ['actions'], {}, '', [
+        ['button', ['action'], { 'data-scene': 'title-section' }, 'Title'],
+        ['button', ['action'], { 'data-scene': 'world-section' }, 'World']
+      ]]
+    ]))
   }
+
   return element
 }
--- types/components/scenes/new-game.d.ts
+++ types/components/scenes/new-game.d.ts
@@ -1,1 +1,8 @@
-export function sectionNewgame(targetElement: any, state: any): any;
+/**
+ * Component to query for player information such as display name.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
+export function sectionNewgame(targetElement: HTMLElement, state: import('../../state/initial-state.js').State): HTMLElement;
