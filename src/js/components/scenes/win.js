a9da1c5044f859cc32a3aeedd97c9ff1840e76fb
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-28 18:19:59 +0200

    chore: add JSDoc comments to Win scene
    I will prep up this scene somewhen in the future, I believe.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/scenes/win.js
+++ src/js/components/scenes/win.js
@@ -1,12 +1,24 @@
 import { el } from '../el.js'
 
+/**
+ * Component to display the victory at the end of the game.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
 export function sectionWin (targetElement, state) {
-  const element = targetElement.cloneNode(true)
+  const element = /** @type {HTMLElement} */(targetElement.cloneNode(true))
   element.innerHTML = ''
+
   if (state.activeScene === 'win-section') {
-    element.appendChild(el('div', [], {}, '', [['h1', [], {}, 'Win'], ['div', ['actions'], {}, '', [['button', ['action'], {
-      'data-scene': 'title-section'
-    }, 'Title']]]]))
+    element.appendChild(el('div', [], {}, '', [
+      ['h1', [], {}, 'Win'],
+      ['div', ['actions'], {}, '', [
+        ['button', ['action'], { 'data-scene': 'title-section' }, 'Title']
+      ]]
+    ]))
   }
+
   return element
 }
--- types/components/scenes/win.d.ts
+++ types/components/scenes/win.d.ts
@@ -1,1 +1,8 @@
-export function sectionWin(targetElement: any, state: any): any;
+/**
+ * Component to display the victory at the end of the game.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
+export function sectionWin(targetElement: HTMLElement, state: import('../../state/initial-state.js').State): HTMLElement;
