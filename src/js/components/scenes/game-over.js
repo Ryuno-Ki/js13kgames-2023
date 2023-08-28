c0c8fd333bf865c7cb7b615dd81562574d809847
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-28 14:41:45 +0200

    chore: add JSDoc comments to game-over scene
    This is a minimal component. I might want to add some stats at the end.
    Like an entry of the history property? Or some charts? Don't know yet.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/scenes/game-over.js
+++ src/js/components/scenes/game-over.js
@@ -1,12 +1,24 @@
 import { el } from '../el.js'
 
+/**
+ * Component to display the game over scene when the game is lost.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
 export function sectionGameOver (targetElement, state) {
-  const element = targetElement.cloneNode(true)
+  const element = /** @type {HTMLElement} */(targetElement.cloneNode(true))
   element.innerHTML = ''
+
   if (state.activeScene === 'game-over-section') {
-    element.appendChild(el('div', [], {}, '', [['h1', [], {}, 'Game Over'], ['div', ['actions'], {}, '', [['button', ['action'], {
-      'data-scene': 'title-section'
-    }, 'Title']]]]))
+    element.appendChild(el('div', [], {}, '', [
+      ['h1', [], {}, 'Game Over'],
+      ['div', ['actions'], {}, '', [
+        ['button', ['action'], { 'data-scene': 'title-section' }, 'Title']
+      ]]
+    ]))
   }
+
   return element
 }
--- types/components/scenes/game-over.d.ts
+++ types/components/scenes/game-over.d.ts
@@ -1,1 +1,8 @@
-export function sectionGameOver(targetElement: any, state: any): any;
+/**
+ * Component to display the game over scene when the game is lost.
+ *
+ * @argument {HTMLElement} targetElement
+ * @argument {import('../../state/initial-state.js').State} state
+ * @returns {HTMLElement}
+ */
+export function sectionGameOver(targetElement: HTMLElement, state: import('../../state/initial-state.js').State): HTMLElement;
