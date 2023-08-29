947c4bd3eb22f90835201de96d47aee8950577de
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 15:07:40 +0200

    test: write tests for switchToCityAction
    Easy, peasy.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/switch-to-city.spec.mjs
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { switchToCityAction } from '../../../src/js/state/actions/switch-to-city.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('switchToCityAction', function () {
+  it('should create a SWITCH_TO_CITY_ACTION', function () {
+    // Arrange
+    const city = 'Lübeck'
+
+    // Act
+    const action = switchToCityAction(city)
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        city: String
+      }
+    })
+    expect(action.type).to.equal('SWITCH_TO_CITY_ACTION')
+    expect(action.payload.city).to.equal(city)
+  })
+})
