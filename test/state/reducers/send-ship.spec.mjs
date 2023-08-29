52f73649238e51c68e28e8a3390889882c3bab2d
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 17:23:52 +0200

    test: write tests for sendShipReducer
    This file was actually one I was working on last week in the bus.
    I realised that my previous approach was too complicated. Since a ship
    can only be on one itinerary at a time I can track it on the ship
    object.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/reducers/send-ship.spec.mjs
@@ -0,0 +1,65 @@
+import chai from 'chai'
+import chaiDeepEqual from 'chai-better-shallow-deep-equal'
+
+import store from '../../../src/js/state/store.js'
+import { sendShipReducer } from '../../../src/js/state/reducers/send-ship.js'
+
+chai.use(chaiDeepEqual)
+const { expect } = chai
+
+describe('sendShipReducer', function () {
+  beforeEach(async function () {
+    await store.dispatch({ type: 'RESET', payload: {} })
+  })
+
+  describe('if ship was not found', function () {
+    it('should not apply any changes', function () {
+      // Arrange
+      const state = store.getState()
+      const payload = { ship: 'Santa Maria' }
+
+      // Act
+      const newState = sendShipReducer(state, payload)
+
+      // Assert
+      expect(newState).not.to.equal(state)
+      expect(newState).to.shallowDeepEqual(state)
+    })
+  })
+
+  describe('if ship was found', function () {
+    it('should remove the position of a ship', function () {
+      // Arrange
+      const state = store.getState()
+      const payload = { from: 'Lübeck', ship: 'Marie', to: 'Wismar' }
+      const shipIndex = state.ships.findIndex((s) => s.name === payload.ship)
+
+      // Act
+      const newState = sendShipReducer(state, payload)
+
+      // Assert
+      expect(newState).not.to.equal(state)
+      expect(newState.ships[shipIndex].position).to.be.null
+      expect(newState.ships[shipIndex].moored).to.be.false
+    })
+
+    it('should track its itinerary', function () {
+      // Arrange
+      const state = store.getState()
+      const payload = { from: 'Lübeck', ship: 'Marie', to: 'Wismar' }
+      const shipIndex = state.ships.findIndex((s) => s.name === payload.ship)
+
+      // Act
+      const newState = sendShipReducer(state, payload)
+
+      // Assert
+      expect(newState).not.to.equal(state)
+      expect(newState.ships[shipIndex].itinerary).to.shallowDeepEqual({
+        from: 'Lübeck',
+        to: 'Wismar',
+        month: Number(state.activeMonth),
+        year: state.activeYear
+      })
+    })
+  })
+})
