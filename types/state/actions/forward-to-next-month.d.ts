0aa1df843404bad3ebafd72a6fbb6dff0c58a2c0
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 14:38:59 +0200

    chore: add JSDoc comments to action
    I am doing it this way so that I can benefit from it once I handle
    reducers.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- types/state/actions/forward-to-next-month.d.ts
+++ types/state/actions/forward-to-next-month.d.ts
@@ -3,15 +3,13 @@
  * @property {'FORWARD_TO_NEXT_MONTH_ACTION'} FORWARD_TO_NEXT_MONTH_ACTION.type
  * @property {object} FORWARD_TO_NEXT_MONTH_ACTION.payload
  */
-
 /**
  * Action creator to allow for forwarding to the next month.
  *
  * @returns {FORWARD_TO_NEXT_MONTH_ACTION}
  */
-export function forwardToNextMonthAction () {
-  return {
-    type: 'FORWARD_TO_NEXT_MONTH_ACTION',
-    payload: {}
-  }
-}
+export function forwardToNextMonthAction(): FORWARD_TO_NEXT_MONTH_ACTION;
+export type FORWARD_TO_NEXT_MONTH_ACTION = {
+    type: 'FORWARD_TO_NEXT_MONTH_ACTION';
+    payload: object;
+};
