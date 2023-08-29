bb02504f146b110aefdbd9424394e3abb8cf8d42
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 15:21:02 +0200

    chore: add JSDoc comments to action
    This might require a refactor with respect to the import path. I've not
    yet decided whether I want to create a barrel import just for this.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/actions/switch-to-scene.js
+++ src/js/state/actions/switch-to-scene.js
@@ -1,3 +1,16 @@
+/**
+ * @typedef {object} SWITCH_TO_SCENE_ACTION
+ * @property {'SWITCH_TO_SCENE_ACTION'} SWITCH_TO_SCENE_ACTION.type
+ * @property {object} SWITCH_TO_SCENE_ACTION.payload
+ * @property {import('../initial-state.js').Scene} SWITCH_TO_SCENE_ACTION.payload.scene
+ */
+
+/**
+ * Action creator to switch to another scene.
+ *
+ * @argument {import('../initial-state.js').Scene} scene
+ * @returns {SWITCH_TO_SCENE_ACTION}
+ */
 export function switchToSceneAction (scene) {
   return {
     type: 'SWITCH_TO_SCENE_ACTION',
--- types/state/actions/switch-to-scene.d.ts
+++ types/state/actions/switch-to-scene.d.ts
@@ -1,6 +1,19 @@
-export function switchToSceneAction(scene: any): {
-    type: string;
+/**
+ * @typedef {object} SWITCH_TO_SCENE_ACTION
+ * @property {'SWITCH_TO_SCENE_ACTION'} SWITCH_TO_SCENE_ACTION.type
+ * @property {object} SWITCH_TO_SCENE_ACTION.payload
+ * @property {import('../initial-state.js').Scene} SWITCH_TO_SCENE_ACTION.payload.scene
+ */
+/**
+ * Action creator to switch to another scene.
+ *
+ * @argument {import('../initial-state.js').Scene} scene
+ * @returns {SWITCH_TO_SCENE_ACTION}
+ */
+export function switchToSceneAction(scene: import('../initial-state.js').Scene): SWITCH_TO_SCENE_ACTION;
+export type SWITCH_TO_SCENE_ACTION = {
+    type: 'SWITCH_TO_SCENE_ACTION';
     payload: {
-        scene: any;
+        scene: import('../initial-state.js').Scene;
     };
 };
