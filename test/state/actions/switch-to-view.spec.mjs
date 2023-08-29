ea7d4e2c93014eaa65740f0edd14a5f7322bc924
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 15:25:15 +0200

    test: write tests for switchToView action
    This is very similar to the previous ones.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/switch-to-view.spec.mjs
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { switchToViewAction } from '../../../src/js/state/actions/switch-to-view.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('switchToViewAction', function () {
+  it('should create a SWITCH_TO_VIEW_ACTION', function () {
+    // Arrange
+    const view = 'city'
+
+    // Act
+    const action = switchToViewAction(view)
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        view: String
+      }
+    })
+    expect(action.type).to.equal('SWITCH_TO_VIEW_ACTION')
+    expect(action.payload.view).to.equal(view)
+  })
+})
