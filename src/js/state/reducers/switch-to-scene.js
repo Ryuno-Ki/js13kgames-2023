95af78ffd9145b3654e73b0dd30ad07689f94e5a
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 17:37:37 +0200

    chore: add JSDoc comments to switchToSceneReducer
    This isn't that complicated.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/reducers/switch-to-scene.js
+++ src/js/state/reducers/switch-to-scene.js
@@ -1,3 +1,10 @@
+/**
+ * Reducer to compute the transition to another scene.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION['payload']} payload
+ * @returns {import('../initial-state.js').State}
+ */
 export function switchToSceneReducer (state, payload) {
   const activeScene = payload.scene
 
