fcf144508a0087265fceed240b787b81b15cd369
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 17:30:49 +0200

    test: write tests for switchToCityReducer
    This one is really easy to test!
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/reducers/switch-to-city.spec.mjs
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+
+import store from '../../../src/js/state/store.js'
+import { switchToCityReducer } from '../../../src/js/state/reducers/switch-to-city.js'
+
+describe('switchToCityReducer', function () {
+  beforeEach(async function () {
+    await store.dispatch({ type: 'RESET', payload: {} })
+  })
+
+  it('should update the active view to "city"', function () {
+    // Arrange
+    const state = store.getState()
+    const payload = { city: 'Wismar' }
+
+    // Act
+    const newState = switchToCityReducer(state, payload)
+
+    // Assert
+    expect(newState).not.to.equal(state)
+    expect(newState.activeView).to.equal('city')
+  })
+
+  it('should update the active city', function () {
+    // Arrange
+    const state = store.getState()
+    const payload = { city: 'Wismar' }
+
+    // Act
+    const newState = switchToCityReducer(state, payload)
+
+    // Assert
+    expect(newState).not.to.equal(state)
+    expect(newState.activeCity).to.equal('Wismar')
+  })
+})
