0b66698c0c94294d1d47e6f33c5fe43b240438a2
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 18:18:26 +0200

    test: write tests for unloadShipReducer
    I even spotted a bug!
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/reducers/unload-ship.spec.mjs
@@ -0,0 +1,96 @@
+import chai from 'chai'
+import chaiDeepEqual from 'chai-better-shallow-deep-equal'
+
+import store from '../../../src/js/state/store.js'
+import { unloadShipReducer } from '../../../src/js/state/reducers/unload-ship.js'
+
+chai.use(chaiDeepEqual)
+const { expect } = chai
+
+describe('unloadShipReducer', function () {
+  beforeEach(async function () {
+    await store.dispatch({ type: 'RESET', payload: {} })
+  })
+
+  describe('if ship was not found', function () {
+    it('should not apply any changes', function () {
+      // Arrange
+      const state = store.getState()
+      const payload = { ship: 'Santa Maria' }
+
+      // Act
+      const newState = unloadShipReducer(state, payload)
+
+      // Assert
+      expect(newState).not.to.equal(state)
+      expect(newState).to.shallowDeepEqual(state)
+    })
+  })
+
+  describe('if ship was found', function () {
+    describe('if cargo was not loaded unto the ship before', function () {
+      it('should not remove it', function () {
+        // Arrange
+        const state = Object.assign({}, store.getState(), { activeCity: 'Lübeck' })
+        const payload = { ship: 'Marie', ware: 'grok', quantity: 42 }
+        const shipIndex = state.ships.findIndex((s) => s.name === payload.ship)
+
+        // Act
+        const newState = unloadShipReducer(state, payload)
+
+        // Assert
+        expect(newState).not.to.equal(state)
+        expect(newState.ships[shipIndex].cargo).to.shallowDeepEqual([])
+      })
+    })
+
+    describe('if cargo was loaded unto the ship before', function () {
+      it('should update its quantity', function () {
+        // Arrange
+        const state = Object.assign(
+          {},
+          store.getState(),
+          {
+            activeCity: 'Lübeck',
+            ships: [{
+              name: 'Marie',
+              moored: true,
+              position: 'Lübeck',
+              cargo: [{ ware: 'grok', quantity: 42 }, { ware: 'wool', quantity: 2 }]
+            }]
+          }
+        )
+        const payload = { ship: 'Marie', ware: 'grok', quantity: 2 }
+        const shipIndex = state.ships.findIndex((s) => s.name === payload.ship)
+
+        // Act
+        const newState = unloadShipReducer(state, payload)
+
+        // Assert
+        expect(newState).not.to.equal(state)
+        expect(newState.ships[shipIndex].cargo).to.shallowDeepEqual([
+          { ware: 'grok', quantity: 40 },
+          { ware: 'wool', quantity: 2 }
+        ])
+      })
+    })
+
+    it('should update the warehouse of that city', function () {
+      // Arrange
+      const state = Object.assign({}, store.getState(), { activeCity: 'Lübeck' })
+      const payload = { city: 'Lübeck', ship: 'Marie', ware: 'wool', quantity: 2 }
+      const cityIndex = state.cities.findIndex((c) => c.name === payload.city)
+
+      // Act
+      const newState = unloadShipReducer(state, payload)
+
+      // Assert
+      expect(newState).not.to.equal(state)
+      expect(newState.cities[cityIndex].supply).to.shallowDeepEqual([
+        { ware: 'honey', quantity: 5 },
+        { ware: 'salt', quantity: 2 },
+        { ware: 'wool', quantity: 12 }
+      ])
+    })
+  })
+})
