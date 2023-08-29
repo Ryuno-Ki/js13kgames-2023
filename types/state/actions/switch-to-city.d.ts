2ba407a14230f9ceb66f4a443a68d6b3400e1038
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 15:09:36 +0200

    chore: add JSDoc comments to action
    This is accepting only a single argument. Therefore I don't need an
    object because there is no order to consider.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- types/state/actions/switch-to-city.d.ts
+++ types/state/actions/switch-to-city.d.ts
@@ -4,18 +4,16 @@
  * @property {object} SWITCH_TO_CITY_ACTION.payload
  * @property {import('../initial-state.js').CityName} SWITCH_TO_CITY_ACTION.payload.city
  */
-
 /**
  * Action creator to switch to another city overview.
  *
  * @argument {import('../initial-state.js').CityName} city
  * @returns {SWITCH_TO_CITY_ACTION}
  */
-export function switchToCityAction (city) {
-  return {
-    type: 'SWITCH_TO_CITY_ACTION',
+export function switchToCityAction(city: import('../initial-state.js').CityName): SWITCH_TO_CITY_ACTION;
+export type SWITCH_TO_CITY_ACTION = {
+    type: 'SWITCH_TO_CITY_ACTION';
     payload: {
-      city
-    }
-  }
-}
+        city: import('../initial-state.js').CityName;
+    };
+};
