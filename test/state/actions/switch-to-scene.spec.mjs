b0085822eeae1d68f2e0500899d910a94dfead6a
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 15:20:00 +0200

    test: write tests for switchToSceneAction
    This is similar to the city pendant.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/switch-to-scene.spec.mjs
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { switchToSceneAction } from '../../../src/js/state/actions/switch-to-scene.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('switchToSceneAction', function () {
+  it('should create a SWITCH_TO_SCENE_ACTION', function () {
+    // Arrange
+    const scene = 'about-section'
+
+    // Act
+    const action = switchToSceneAction(scene)
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        scene: String
+      }
+    })
+    expect(action.type).to.equal('SWITCH_TO_SCENE_ACTION')
+    expect(action.payload.scene).to.equal(scene)
+  })
+})
