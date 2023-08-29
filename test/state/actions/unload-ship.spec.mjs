a3fa17e47392eaa397370a89eb9085315b25843f
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-29 15:29:18 +0200

    test: write tests for unloadShipAction
    This is the pendant to loadShipAction.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/unload-ship.spec.mjs
@@ -0,0 +1,39 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { unloadShipAction } from '../../../src/js/state/actions/unload-ship.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('unloadShipAction', function () {
+  it('should create a UNLOAD_SHIP_ACTION', function () {
+    // Arrange
+    const city = 'Lübeck'
+    const ship = 'Santa Maria'
+    const ware = 'grok'
+    const quantity = 42
+
+    // Act
+    const action = unloadShipAction({ city, ship, ware, quantity })
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        city: String,
+        ship: String,
+        ware: String,
+        quantity: Number
+      }
+    })
+    expect(action.type).to.equal('UNLOAD_SHIP_ACTION')
+    expect(action.payload.city).to.equal(city)
+    expect(action.payload.ship).to.equal(ship)
+    expect(action.payload.ware).to.equal(ware)
+    expect(action.payload.quantity).to.equal(quantity)
+  })
+})
