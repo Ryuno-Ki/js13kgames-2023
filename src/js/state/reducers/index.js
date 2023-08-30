32c09a2f23acf6230954c1d13d15b4eef560a921
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-30 23:40:58 +0200

    chore: add JSDoc comments to reducer
    This is ugly but it works.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/reducers/index.js
+++ src/js/state/reducers/index.js
@@ -8,41 +8,72 @@ import { switchToViewReducer } from './switch-to-view.js'
 import { unloadShipReducer } from './unload-ship.js'
 import { updateShipsReducer } from './update-ships.js'
 
+/**
+ * Combined reducer.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
+ * @returns {import('../initial-state.js').State}
+ */
 export function reducer (state, action) {
   if (typeof state === 'undefined') return initialState
   const type = action.type
   const payload = action.payload
 
   if (type === 'FORWARD_TO_NEXT_MONTH_ACTION') {
-    return forwardToNextMonthReducer(state, payload)
+    return forwardToNextMonthReducer(
+      state,
+      /** @type {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION['payload']} */(payload)
+    )
   }
 
   if (type === 'LOAD_SHIP_ACTION') {
-    return loadShipReducer(state, payload)
+    return loadShipReducer(
+      state,
+      /** @type {import('../actions/load-ship.js').LOAD_SHIP_ACTION['payload']} */(payload)
+    )
   }
 
   if (type === 'SEND_SHIP_ACTION') {
-    return sendShipReducer(state, payload)
+    return sendShipReducer(
+      state,
+      /** @type {import('../actions/send-ship.js').SEND_SHIP_ACTION['payload']} */(payload)
+    )
   }
 
   if (type === 'SWITCH_TO_CITY_ACTION') {
-    return switchToCityReducer(state, payload)
+    return switchToCityReducer(
+      state,
+      /** @type {import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION['payload']} */(payload)
+    )
   }
 
   if (type === 'SWITCH_TO_SCENE_ACTION') {
-    return switchToSceneReducer(state, payload)
+    return switchToSceneReducer(
+      state,
+      /** @type {import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION['payload']} */(payload)
+    )
   }
 
   if (type === 'SWITCH_TO_VIEW_ACTION') {
-    return switchToViewReducer(state, payload)
+    return switchToViewReducer(
+      state,
+      /** @type {import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION['payload']} */(payload)
+    )
   }
 
   if (type === 'UNLOAD_SHIP_ACTION') {
-    return unloadShipReducer(state, payload)
+    return unloadShipReducer(
+      state,
+      /** @type {import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION['payload']} */(payload)
+    )
   }
 
-  if (type === 'UPDATE-SHIPS_ACTION') {
-    return updateShipsReducer(state, payload)
+  if (type === 'UPDATE_SHIPS_ACTION') {
+    return updateShipsReducer(
+      state,
+      /** @type {import('../actions/update-ships.js').UPDATE_SHIPS_ACTION['payload']} */(payload)
+    )
   }
 
   return state
--- types/state/reducers/index.d.ts
+++ types/state/reducers/index.d.ts
@@ -1,1 +1,8 @@
-export function reducer(state: any, action: any): any;
+/**
+ * Combined reducer.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
+ * @returns {import('../initial-state.js').State}
+ */
+export function reducer(state: import('../initial-state.js').State, action: import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION): import('../initial-state.js').State;
