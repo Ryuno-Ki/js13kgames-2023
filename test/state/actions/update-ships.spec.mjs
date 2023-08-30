c54a254c86e721c3efc11faa4cbb1b0cd8107191
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-30 22:53:37 +0200

    test: write tests for updateShipsAction
    This is merely meant to defer to the correct reducer.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/update-ships.spec.mjs
@@ -0,0 +1,27 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { updateShipsAction } from '../../../src/js/state/actions/update-ships.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('updateShipsAction', function () {
+  it('should create a UPDATE_SHIPS_ACTION', function () {
+    // Arrange
+    // Nothing to prepare
+
+    // Act
+    const action = updateShipsAction()
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {}
+    })
+    expect(action.type).to.equal('UPDATE_SHIPS_ACTION')
+  })
+})
