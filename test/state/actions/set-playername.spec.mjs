83444bd2a3bcb571213a9456e96be51e2a64f363
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 13:38:30 +0200

    test: write tests for setPlayernameAction
    This action will pass the entered playername to the reducer.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/set-playername.spec.mjs
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { setPlayernameAction } from '../../../src/js/state/actions/set-playername.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('setPlayernameAction', function () {
+  it('should create a SET_PLAYERNAME_ACTION', function () {
+    // Arrange
+    const playername = 'Christoph'
+
+    // Act
+    const action = setPlayernameAction(playername)
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        playername: String
+      }
+    })
+    expect(action.type).to.equal('SET_PLAYERNAME_ACTION')
+    expect(action.payload.playername).to.equal(playername)
+  })
+})
