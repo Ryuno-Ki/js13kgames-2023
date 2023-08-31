70ff2f56065d7404cad99cc86fc8aaa874d0bba8
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 13:39:13 +0200

    feat: add setPlayernameAction
    I want to allow players to pick a name to feel more immersed.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/state/actions/set-playername.js
@@ -0,0 +1,21 @@
+/**
+ * @typedef {object} SET_PLAYERNAME_ACTION
+ * @property {'SET_PLAYERNAME_ACTION'} SET_PLAYERNAME_ACTION.type
+ * @property {object} SET_PLAYERNAME_ACTION.payload
+ * @property {string} SET_PLAYERNAME_ACTION.payload.playername
+ */
+
+/**
+ * Action creator to set name of the player.
+ *
+ * @argument {string} playername
+ * @returns {SET_PLAYERNAME_ACTION}
+ */
+export function setPlayernameAction (playername) {
+  return {
+    type: 'SET_PLAYERNAME_ACTION',
+    payload: {
+      playername
+    }
+  }
+}
