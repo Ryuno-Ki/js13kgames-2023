5145b6bd2d264332b444cfdacf45ca171efc8ff2
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 20:47:25 +0200

    feat: add checkOnWinConditionReducer
    This is my first approach: wait long enough.
    
    I plan to introduce a currency (silver coin?) eventually together with a
    little story. It goes as this: At the beginning of your adventure some
    fellow merchants invests in you and expects a return a year and a month
    (= 13 months) later. You win when you have made more money than with
    what you started with, otherwise you loose.
    
    For the free play I want to have the end of the game once you enter a
    new century. I have not spend time thinking about how I want to evaluate
    the win here.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/state/reducers/check-on-win-condition.js
@@ -0,0 +1,27 @@
+/**
+ * Reducer to check on win condition based on scenario.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION['payload']} payload
+ * @returns {import('../initial-state.js').State}
+ */
+export function checkOnWinConditionReducer (state, payload) {
+  let { activeScene } = state
+  const { activeMonth, activeScenario, activeYear } = state
+
+  if (activeScenario === 'tutorial') {
+    // TODO: Update if I decide to pick another start year
+    if (activeYear >= 1251 && Number(activeMonth) >= 2) {
+      // TODO: Check on balance. Should exceed the starting value.
+      activeScene = 'win-section'
+    }
+  }
+
+  if (activeScenario === 'free-play') {
+    if (activeYear >= 1300) {
+      activeScene = 'win-section'
+    }
+  }
+
+  return Object.assign({}, state, { activeScene })
+}
---
+++ types/state/reducers/check-on-win-condition.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Reducer to check on win condition based on scenario.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION['payload']} payload
+ * @returns {import('../initial-state.js').State}
+ */
+export function checkOnWinConditionReducer(state: import('../initial-state.js').State, payload: import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION['payload']): import('../initial-state.js').State;
