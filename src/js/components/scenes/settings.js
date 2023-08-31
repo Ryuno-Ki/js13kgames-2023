5c54eb1f3df46d877bb5bcf41865f312bdb75610
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 11:59:59 +0200

    style: add CSS to settings scene
    This is now presented more appealing in my opinion. Time to make the
    select work!
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/scenes/settings.js
+++ src/js/components/scenes/settings.js
@@ -14,11 +14,18 @@ export function sectionSettings (targetElement, state) {
   if (state.activeScene === 'settings-section') {
     element.appendChild(el('div', [], {}, '', [
       ['h1', [], {}, 'Settings'],
-      ['label', [], { for: 'color-preference' }, 'Choose your theme'],
-      ['select', [], { id: 'color-preference' }, '', [
-        ['option', [], { value: 'system', checked: 'checked' }, 'System'],
-        ['option', [], { value: 'light' }, 'Light'],
-        ['option', [], { value: 'dark' }, 'Dark']
+      ['fieldset', [], {}, '', [
+        ['legend', [], {}, 'Visual Interface'],
+        ['div', [], {}, '', [
+          ['label', [], { for: 'color-preference' }, 'Choose your theme']
+        ]],
+        ['div', [], {}, '', [
+          ['select', [], { id: 'color-preference' }, '', [
+            ['option', [], { value: 'system', checked: 'checked' }, 'System'],
+            ['option', [], { value: 'light' }, 'Light'],
+            ['option', [], { value: 'dark' }, 'Dark']
+          ]]
+        ]]
       ]],
       ['div', ['actions'], {}, '', [
         ['button', ['action'], { 'data-scene': 'title-section' }, 'Title']
