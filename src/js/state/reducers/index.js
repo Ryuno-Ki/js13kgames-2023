664989dfdf3102237cef46fab22bd13c0bcdd0be
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 14:22:04 +0200

    feat: announce setLevelScenarioReducer to the game
    Otherwise the state won't update.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/reducers/index.js
+++ src/js/state/reducers/index.js
@@ -5,6 +5,7 @@ import { resetReducer } from './reset.js'
 import { sendShipReducer } from './send-ship.js'
 import { setColorPreferenceReducer } from './set-color-preference.js'
 import { setPlayernameReducer } from './set-playername.js'
+import { setLevelScenarioReducer } from './set-level-scenario.js'
 import { switchToCityReducer } from './switch-to-city.js'
 import { switchToSceneReducer } from './switch-to-scene.js'
 import { switchToViewReducer } from './switch-to-view.js'
@@ -15,7 +16,7 @@ import { updateShipsReducer } from './update-ships.js'
  * Combined reducer.
  *
  * @argument {import('../initial-state.js').State | undefined} state
- * @argument {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/reset.js').RESET_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION | import('../actions/set-playername.js').SET_PLAYERNAME_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
+ * @argument {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/reset.js').RESET_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION | import('../actions/set-level-scenario.js').SET_LEVEL_SCENARIO_ACTION | import('../actions/set-playername.js').SET_PLAYERNAME_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
  * @returns {import('../initial-state.js').State}
  */
 export function reducer (state, action) {
@@ -57,6 +58,13 @@ export function reducer (state, action) {
     return setColorPreferenceReducer(
       state,
       /** @type {import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION['payload']} */(payload)
+    )
+  }
+
+  if (type === 'SET_LEVEL_SCENARIO_ACTION') {
+    return setLevelScenarioReducer(
+      state,
+      /** @type {import('../actions/set-level-scenario.js').SET_LEVEL_SCENARIO_ACTION['payload']} */(payload)
     )
   }
 
