3d577ca4e91e2ba600f43853446b69956b6f9bbe
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 11:08:49 +0200

    feat: write a new reducer for state reset
    I've added JSDoc comments at the same time.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/state/reducers/reset.js
@@ -0,0 +1,12 @@
+import { initialState } from '../initial-state.js'
+
+/**
+ * Reducer to reset the state.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/reset.js').RESET_ACTION['payload']} payload
+ * @returns {import('../initial-state.js').State}
+ */
+export function resetReducer (state, payload) {
+  return Object.assign({}, initialState)
+}
