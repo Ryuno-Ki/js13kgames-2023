8e674e9444fb74a1c05b3daf9d3a92e8f1169498
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 11:04:56 +0200

    feat: create new action for state resets
    This is soon used in the store as well.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/state/actions/reset.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {object} RESET_ACTION
+ * @property {'RESET_ACTION'} RESET_ACTION.type
+ * @property {object} RESET_ACTION.payload
+ */
+
+/**
+ * Action creator to reset the complete state.
+ *
+ * @returns {RESET_ACTION}
+ */
+export function resetAction () {
+  return {
+    type: 'RESET_ACTION',
+    payload: {}
+  }
+}
