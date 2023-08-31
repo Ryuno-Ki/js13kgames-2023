cb5cb6f8d75eb20d7f8d721c0efb72fe90b339f9
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 12:14:22 +0200

    test: write tests for setColorPreferenceAction
    This is triggering a state update on color preference.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/set-color-preference.spec.mjs
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { setColorPreferenceAction } from '../../../src/js/state/actions/set-color-preference.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('setColorPreferenceAction', function () {
+  it('should create a SET_COLOR_PREFERENCE_ACTION', function () {
+    // Arrange
+    const color = 'dark'
+
+    // Act
+    const action = setColorPreferenceAction(color)
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        color: String
+      }
+    })
+    expect(action.type).to.equal('SET_COLOR_PREFERENCE_ACTION')
+    expect(action.payload.color).to.equal(color)
+  })
+})
