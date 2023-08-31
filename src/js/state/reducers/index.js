561c160126058986d3fe75374bb8c355e9050656
André Jaenisch <andre.jaenisch@posteo.de>
2023-08-31 11:10:04 +0200

    refactor: use reset action creator and reducer
    To finish things off I need to swap some parts in the implementation.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/reducers/index.js
+++ src/js/state/reducers/index.js
@@ -1,6 +1,7 @@
 import { initialState } from '../initial-state.js'
 import { forwardToNextMonthReducer } from './forward-to-next-month.js'
 import { loadShipReducer } from './load-ship.js'
+import { resetReducer } from './reset.js'
 import { sendShipReducer } from './send-ship.js'
 import { switchToCityReducer } from './switch-to-city.js'
 import { switchToSceneReducer } from './switch-to-scene.js'
@@ -12,7 +13,7 @@ import { updateShipsReducer } from './update-ships.js'
  * Combined reducer.
  *
  * @argument {import('../initial-state.js').State | undefined} state
- * @argument {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
+ * @argument {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/reset.js').RESET_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
  * @returns {import('../initial-state.js').State}
  */
 export function reducer (state, action) {
@@ -33,6 +34,13 @@ export function reducer (state, action) {
     return loadShipReducer(
       state,
       /** @type {import('../actions/load-ship.js').LOAD_SHIP_ACTION['payload']} */(payload)
+    )
+  }
+
+  if (type === 'RESET_ACTION') {
+    return resetReducer(
+      state,
+      /** @type {import('../actions/reset.js').RESET_ACTION['payload']} */(payload)
     )
   }
 
