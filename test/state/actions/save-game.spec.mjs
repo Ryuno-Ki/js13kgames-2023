fcce15530a94d83f558f5b23fd37cdc2ee936dc2
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-01 20:52:24 +0200

    test: write tests for save game action
    I realised that piggy-backing on another action would cause too much
    technical debt. Instead I create an action without a matching reducer
    here so that I can react on it within the Store.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/save-game.spec.mjs
@@ -0,0 +1,27 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { saveGameAction } from '../../../src/js/state/actions/save-game.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('saveGameAction', function () {
+  it('should create a SAVE_GAME_ACTION', function () {
+    // Arrange
+    // Nothing to prepare
+
+    // Act
+    const action = saveGameAction()
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {}
+    })
+    expect(action.type).to.equal('SAVE_GAME_ACTION')
+  })
+})
