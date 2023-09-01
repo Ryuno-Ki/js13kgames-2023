1cef06c10d0b5023c6588baeec3840aeea5d8c5d
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-01 18:12:03 +0200

    refactor: implement fake local storage
    In order to have a testable localStorage I fake it as plain object in
    memory. This requires a manipulation via functions that have access to
    this local variable.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- test/require.js
+++ test/require.js
@@ -1,6 +1,7 @@
 import { JSDOM } from 'jsdom'
 
 const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>', { url: 'https://jaenis.ch/' })
+const fakeStorage = {}
 
 global.document = dom.window.document
 
@@ -12,8 +13,15 @@ global.window = {
   },
   Event: dom.window.Event,
   localStorage: {
-    getItem: () => null,
-    setItem: () => {}
+    setItem: (key, value) => {
+      fakeStorage[key] = value
+    },
+    getItem: (key) => {
+      return fakeStorage[key] || null
+    },
+    removeItem: (key) => {
+      delete fakeStorage[key]
+    }
   },
   requestAnimationFrame: () => {}
 }
