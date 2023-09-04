2a2345330e5aa3507c7989168a99d34fb45cc255
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-04 16:01:30 +0200

    test: write tests for delete of a game
    I identify games by playername.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/delete-game.spec.mjs
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { deleteGameAction } from '../../../src/js/state/actions/delete-game.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('deleteGameAction', function () {
+  it('should create a DELETE_GAME_ACTION', function () {
+    // Arrange
+    const playername = 'Kolumbus'
+
+    // Act
+    const action = deleteGameAction(playername)
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        playername: String
+      }
+    })
+    expect(action.type).to.equal('DELETE_GAME_ACTION')
+    expect(action.payload.playername).to.equal(playername)
+  })
+})
