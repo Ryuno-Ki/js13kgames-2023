04cf73e5f222cd403f0dda84bed3fab7e512a865
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-04 15:35:07 +0200

    feat: announce loadGameReducer to game
    Otherwise treeshaking will remove it from the bundle.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/reducers/index.js
+++ src/js/state/reducers/index.js
@@ -1,6 +1,7 @@
 import { initialState } from '../initial-state.js'
 import { checkOnWinConditionReducer } from './check-on-win-condition.js'
 import { forwardToNextMonthReducer } from './forward-to-next-month.js'
+import { loadGameReducer } from './load-game.js'
 import { loadShipReducer } from './load-ship.js'
 import { resetReducer } from './reset.js'
 import { sendShipReducer } from './send-ship.js'
@@ -17,7 +18,7 @@ import { updateShipsReducer } from './update-ships.js'
  * Combined reducer.
  *
  * @argument {import('../initial-state.js').State | undefined} state
- * @argument {import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION | import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/reset.js').RESET_ACTION | import('../actions/save-game.js').SAVE_GAME_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION | import('../actions/set-level-scenario.js').SET_LEVEL_SCENARIO_ACTION | import('../actions/set-playername.js').SET_PLAYERNAME_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
+ * @argument {import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION | import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-game.js').LOAD_GAME_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/reset.js').RESET_ACTION | import('../actions/save-game.js').SAVE_GAME_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION | import('../actions/set-level-scenario.js').SET_LEVEL_SCENARIO_ACTION | import('../actions/set-playername.js').SET_PLAYERNAME_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
  * @returns {import('../initial-state.js').State}
  */
 export function reducer (state, action) {
@@ -38,6 +39,13 @@ export function reducer (state, action) {
     return forwardToNextMonthReducer(
       state,
       /** @type {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION['payload']} */(payload)
+    )
+  }
+
+  if (type === 'LOAD_GAME_ACTION') {
+    return loadGameReducer(
+      state,
+      /** @type {import('../actions/load-game.js').LOAD_GAME_ACTION['payload']} */(payload)
     )
   }
 
