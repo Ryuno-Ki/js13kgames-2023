bc41925ba53e230b312ba12ea256f2728aacc916
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-05 14:05:26 +0200

    refactor: remove superfluous attributes
    Now I need to update the city-overview component to avoid passing data
    that is no longer needed.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/city-overview.js
+++ src/js/components/city-overview.js
@@ -16,15 +16,11 @@ export function cityOverview (targetElement, state) {
     return element
   }
 
-  const destinations = state.cities.filter((city) => city.name !== state.activeCity)
-  const ships = state.ships
-    .filter((ship) => ship.position === state.activeCity)
-    .filter((ship) => ship.moored)
-
   element.innerHTML = ''
+
   element.appendChild(el('div', [], {}, '', [
     ['div', [], {}, city.name],
-    ['div', [], { 'data-component': 'docks', 'data-destinations': JSON.stringify(destinations), 'data-ships': JSON.stringify(ships), 'data-warehouse': JSON.stringify(city.supply) }],
+    ['div', [], { 'data-component': 'docks' }],
     ['div', [], { 'data-component': 'market', 'data-type': 'Supply', 'data-wares': JSON.stringify(city.supply) }],
     ['div', [], { 'data-component': 'market', 'data-type': 'Demand', 'data-wares': JSON.stringify(city.demand) }],
     ['button', [], { type: 'button', 'data-view': 'sea' }, 'See other city']
