42e3f831f41af717d8b177e00e542fe1635e3b13
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 15:22:08 +0200

    feat: add setTutorialAction
    This is fired on the same time the scene is changed. If the scene is
    level, I must set the flags. I could dispatch the action on input of the
    select as well but I expect this to happen more often than transition
    between scenes.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/state/actions/set-tutorial.js
@@ -0,0 +1,21 @@
+/**
+ * @typedef {object} SET_TUTORIAL_ACTION
+ * @property {'SET_TUTORIAL_ACTION'} SET_TUTORIAL_ACTION.type
+ * @property {object} SET_TUTORIAL_ACTION.payload
+ * @property {import('../initial-state.js').Scene} SET_TUTORIAL_ACTION.payload.scene
+ */
+
+/**
+ * Action creator to set the tutorial flags.
+ *
+ * @argument {import('../initial-state.js').Scene} scene
+ * @returns {SET_TUTORIAL_ACTION}
+ */
+export function setTutorialAction (scene) {
+  return {
+    type: 'SET_TUTORIAL_ACTION',
+    payload: {
+      scene
+    }
+  }
+}
