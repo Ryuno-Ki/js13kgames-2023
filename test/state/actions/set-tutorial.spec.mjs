9107f1726d86bdba23832fe0cc93c6ca4ca1e3e0
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 15:21:17 +0200

    test: write tests for setTutorialAction
    I want to differentiate between the tutorial and the free-play world and
    this is the way they will do so.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/set-tutorial.spec.mjs
@@ -0,0 +1,30 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { setTutorialAction } from '../../../src/js/state/actions/set-tutorial.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('setTutorialAction', function () {
+  it('should create a SET_TUTORIAL_ACTION', function () {
+    // Arrange
+    const scene = 'about-section'
+
+    // Act
+    const action = setTutorialAction(scene)
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        scene: String
+      }
+    })
+    expect(action.type).to.equal('SET_TUTORIAL_ACTION')
+    expect(action.payload.scene).to.equal(scene)
+  })
+})
