e59bfc757cf215d1e78747f368a80b8d1c0ef019
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 18:28:03 +0200

    test: write tests for sellReducer
    This is similar but not identical to the buyReducer.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/reducers/sell.spec.mjs
@@ -0,0 +1,77 @@
+import chai from 'chai'
+import chaiDeepEqual from 'chai-better-shallow-deep-equal'
+
+import { resetAction } from '../../../src/js/state/actions/reset.js'
+import store from '../../../src/js/state/store.js'
+import { sellReducer } from '../../../src/js/state/reducers/sell.js'
+
+chai.use(chaiDeepEqual)
+const { expect } = chai
+
+describe('sellReducer', function () {
+  beforeEach(async function () {
+    await store.dispatch(resetAction())
+  })
+
+  describe('if ware was not sold before', function () {
+    it('should add it as a new supply', function () {
+      // Arrange
+      const cities = [{ name: 'Lübeck', warehouse: [{ ware: 'grok', quantity: 42 }], supply: [] }]
+      const state = Object.assign({}, store.getState(), { activeCity: 'Lübeck', cities })
+      const payload = { city: 'Lübeck', ware: 'grok', quantity: 42 }
+      const cityIndex = state.cities.findIndex((c) => c.name === payload.city)
+
+      // Act
+      const newState = sellReducer(state, payload)
+
+      // Assert
+      expect(newState).not.to.equal(state)
+      expect(newState.cities[cityIndex].supply).to.shallowDeepEqual([{ ware: 'grok', quantity: 42 }])
+    })
+  })
+
+  describe('if ware was sold before', function () {
+    it('should update its quantity', function () {
+      // Arrange
+      const state = Object.assign(
+        {},
+        store.getState(),
+        {
+          activeCity: 'Lübeck',
+          cities: [{
+            name: 'Lübeck',
+            supply: [{ ware: 'grok', quantity: 2 }],
+            warehouse: [{ ware: 'grok', quantity: 42 }]
+          }]
+        }
+      )
+      const payload = { city: 'Lübeck', ware: 'grok', quantity: 2 }
+      const cityIndex = state.cities.findIndex((c) => c.name === payload.city)
+
+      // Act
+      const newState = sellReducer(state, payload)
+
+      // Assert
+      expect(newState).not.to.equal(state)
+      expect(newState.cities[cityIndex].warehouse).to.shallowDeepEqual([{ ware: 'grok', quantity: 40 }])
+    })
+  })
+
+  it('should update the market of that city', function () {
+    // Arrange
+    const state = Object.assign({}, store.getState(), { activeCity: 'Lübeck' })
+    const payload = { city: 'Lübeck', ware: 'wool', quantity: 2 }
+    const cityIndex = state.cities.findIndex((c) => c.name === payload.city)
+
+    // Act
+    const newState = sellReducer(state, payload)
+
+    // Assert
+    expect(newState).not.to.equal(state)
+    expect(newState.cities[cityIndex].supply).to.shallowDeepEqual([
+      { ware: 'honey', quantity: 5 },
+      { ware: 'salt', quantity: 2 },
+      { ware: 'wool', quantity: 12 }
+    ])
+  })
+})
