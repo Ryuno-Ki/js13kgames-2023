b5ba0f204df7c935172ea993a901430c21c2f15d
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 15:02:14 +0200

    feat: add checkOnGameoverConditionAction
    This is shaped similar to the win conditon for now.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/state/actions/check-on-gameover-condition.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {object} CHECK_ON_GAMEOVER_CONDITION_ACTION
+ * @property {'CHECK_ON_GAMEOVER_CONDITION_ACTION'} CHECK_ON_GAMEOVER_CONDITION_ACTION.type
+ * @property {object} CHECK_ON_GAMEOVER_CONDITION_ACTION.payload
+ */
+
+/**
+ * Action creator to check on gameover condition.
+ *
+ * @returns {CHECK_ON_GAMEOVER_CONDITION_ACTION}
+ */
+export function checkOnGameoverConditionAction () {
+  return {
+    type: 'CHECK_ON_GAMEOVER_CONDITION_ACTION',
+    payload: {}
+  }
+}
---
+++ types/state/actions/check-on-gameover-condition.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @typedef {object} CHECK_ON_GAMEOVER_CONDITION_ACTION
+ * @property {'CHECK_ON_GAMEOVER_CONDITION_ACTION'} CHECK_ON_GAMEOVER_CONDITION_ACTION.type
+ * @property {object} CHECK_ON_GAMEOVER_CONDITION_ACTION.payload
+ */
+/**
+ * Action creator to check on gameover condition.
+ *
+ * @returns {CHECK_ON_GAMEOVER_CONDITION_ACTION}
+ */
+export function checkOnGameoverConditionAction(): CHECK_ON_GAMEOVER_CONDITION_ACTION;
+export type CHECK_ON_GAMEOVER_CONDITION_ACTION = {
+    type: 'CHECK_ON_GAMEOVER_CONDITION_ACTION';
+    payload: object;
+};
