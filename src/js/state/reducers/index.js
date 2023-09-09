1be8e16351d639ec87140b3e4c122db755dae1ae
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 15:41:26 +0200

    feat: announce setTutorialReducer
    Otherwise I cannot invoke it.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/reducers/index.js
+++ src/js/state/reducers/index.js
@@ -7,8 +7,9 @@ import { loadShipReducer } from './load-ship.js'
 import { resetReducer } from './reset.js'
 import { sendShipReducer } from './send-ship.js'
 import { setColorPreferenceReducer } from './set-color-preference.js'
-import { setPlayernameReducer } from './set-playername.js'
 import { setLevelScenarioReducer } from './set-level-scenario.js'
+import { setPlayernameReducer } from './set-playername.js'
+import { setTutorialReducer } from './set-tutorial.js'
 import { switchToCityReducer } from './switch-to-city.js'
 import { switchToSceneReducer } from './switch-to-scene.js'
 import { switchToViewReducer } from './switch-to-view.js'
@@ -19,7 +20,7 @@ import { updateShipsReducer } from './update-ships.js'
  * Combined reducer.
  *
  * @argument {import('../initial-state.js').State | undefined} state
- * @argument {import('../actions/check-on-gameover-condition.js').CHECK_ON_GAMEOVER_CONDITION_ACTION | import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION | import('../actions/delete-game.js').DELETE_GAME_ACTION | import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-game.js').LOAD_GAME_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/reset.js').RESET_ACTION | import('../actions/save-game.js').SAVE_GAME_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION | import('../actions/set-level-scenario.js').SET_LEVEL_SCENARIO_ACTION | import('../actions/set-playername.js').SET_PLAYERNAME_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
+ * @argument {import('../actions/check-on-gameover-condition.js').CHECK_ON_GAMEOVER_CONDITION_ACTION | import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION | import('../actions/delete-game.js').DELETE_GAME_ACTION | import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION | import('../actions/load-game.js').LOAD_GAME_ACTION | import('../actions/load-ship.js').LOAD_SHIP_ACTION | import('../actions/reset.js').RESET_ACTION | import('../actions/save-game.js').SAVE_GAME_ACTION | import('../actions/send-ship.js').SEND_SHIP_ACTION | import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION | import('../actions/set-level-scenario.js').SET_LEVEL_SCENARIO_ACTION | import('../actions/set-playername.js').SET_PLAYERNAME_ACTION | import('../actions/set-tutorial.js').SET_TUTORIAL_ACTION | import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION | import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION | import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION | import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION | import('../actions/update-ships.js').UPDATE_SHIPS_ACTION} action
  * @returns {import('../initial-state.js').State}
  */
 export function reducer (state, action) {
@@ -101,6 +102,13 @@ export function reducer (state, action) {
     return setPlayernameReducer(
       state,
       /** @type {import('../actions/set-playername.js').SET_PLAYERNAME_ACTION['payload']} */(payload)
+    )
+  }
+
+  if (type === 'SET_TUTORIAL_ACTION') {
+    return setTutorialReducer(
+      state,
+      /** @type {import('../actions/set-tutorial.js').SET_TUTORIAL_ACTION['payload']} */(payload)
     )
   }
 
