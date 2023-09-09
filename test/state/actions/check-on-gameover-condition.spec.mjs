a1864d79ced597177ef95d25140e5b3a80950c01
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 15:01:34 +0200

    test: write tests for gameover checks
    I want to remove the gameover button from the level scene. Therefore I
    need to run the checks elsewhere so that I can transition to that scene
    if needed.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/check-on-gameover-condition.spec.mjs
@@ -0,0 +1,27 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { checkOnGameoverConditionAction } from '../../../src/js/state/actions/check-on-gameover-condition.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('checkOnGameoverConditionAction', function () {
+  it('should create a CHECK_ON_GAMEOVER_CONDITION_ACTION', function () {
+    // Arrange
+    // Nothing to prepare
+
+    // Act
+    const action = checkOnGameoverConditionAction()
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {}
+    })
+    expect(action.type).to.equal('CHECK_ON_GAMEOVER_CONDITION_ACTION')
+  })
+})
