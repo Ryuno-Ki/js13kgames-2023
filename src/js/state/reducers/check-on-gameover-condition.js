91c093a8a33bc49c407adf719947bc59309f7a3f
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 15:06:52 +0200

    feat: add checkOnGameoverConditionReducer
    I noticed that my notation on GameOver is inconsistent. Yet I'm not
    willing to harmonise it at this point in time. I have more urgent needs
    to address.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/state/reducers/check-on-gameover-condition.js
@@ -0,0 +1,27 @@
+/**
+ * Reducer to check on gameover condition based on scenario.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/check-on-gameover-condition.js').CHECK_ON_GAMEOVER_CONDITION_ACTION['payload']} payload
+ * @returns {import('../initial-state.js').State}
+ */
+export function checkOnGameoverConditionReducer (state, payload) {
+  let { activeScene } = state
+  const { activeMonth, activeScenario, activeYear } = state
+
+  if (activeScenario === 'tutorial') {
+    // TODO: Update if I decide to pick another start year
+    if (activeYear >= 1251 && Number(activeMonth) >= 2) {
+      // TODO: Check on balance. Should exceed the starting value.
+      activeScene = 'game-over-section'
+    }
+  }
+
+  if (activeScenario === 'free-play') {
+    if (activeYear >= 1300) {
+      activeScene = 'game-over-section'
+    }
+  }
+
+  return Object.assign({}, state, { activeScene })
+}
---
+++ types/state/reducers/check-on-gameover-condition.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Reducer to check on gameover condition based on scenario.
+ *
+ * @argument {import('../initial-state.js').State} state
+ * @argument {import('../actions/check-on-gameover-condition.js').CHECK_ON_GAMEOVER_CONDITION_ACTION['payload']} payload
+ * @returns {import('../initial-state.js').State}
+ */
+export function checkOnGameoverConditionReducer(state: import('../initial-state.js').State, payload: import('../actions/check-on-gameover-condition.js').CHECK_ON_GAMEOVER_CONDITION_ACTION['payload']): import('../initial-state.js').State;
