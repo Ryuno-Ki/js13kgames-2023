4d26928fd3b496fcfe3cf6939a0f3109ce8e63f7
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-09 18:20:04 +0200

    test: write tests for sellAction
    This is going to be the pendant to the buy action.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/sell.spec.mjs
@@ -0,0 +1,36 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { sellAction } from '../../../src/js/state/actions/sell.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('sellAction', function () {
+  it('should create a SELL_ACTION', function () {
+    // Arrange
+    const city = 'Lübeck'
+    const ware = 'grok'
+    const quantity = 42
+
+    // Act
+    const action = sellAction({ city, ware, quantity })
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {
+        city: String,
+        ware: String,
+        quantity: Number
+      }
+    })
+    expect(action.type).to.equal('SELL_ACTION')
+    expect(action.payload.city).to.equal(city)
+    expect(action.payload.ware).to.equal(ware)
+    expect(action.payload.quantity).to.equal(quantity)
+  })
+})
