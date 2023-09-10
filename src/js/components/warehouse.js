3bb9e8697986d408246915eb3549845fe351ffb6
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-10 17:29:07 +0200

    feat: show warehouse stock
    The component felt so empty until I realised I don't display what's in
    stock yet.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/components/warehouse.js
+++ src/js/components/warehouse.js
@@ -11,8 +11,18 @@ export function warehouse (targetElement, state) {
   const element = /** @type {HTMLElement} */(targetElement.cloneNode(true))
   element.innerHTML = ''
 
+  const city = state.cities.find((city) => city.name === state.activeCity)
+  if (!city) {
+    console.warn('Could not find city')
+    return element
+  }
+
   element.appendChild(el('div', [], {}, '', [
     ['div', [], { 'data-component': 'tutorial' }],
+    ['p', [], {}, 'This is your warehouse. Organise your activities here.'],
+    ['ul', [], {}, '', city.warehouse.stock.map((ware) => [
+      'li', [], {}, `${ware.ware} (${ware.quantity})`
+    ])],
     ['button', [], { type: 'button', 'data-view': 'docks' }, 'To the docks'],
     ['button', [], { type: 'button', 'data-view': 'market' }, 'To the market'],
     ['button', [], { type: 'button', 'data-view': 'sea' }, 'To the sea map']
