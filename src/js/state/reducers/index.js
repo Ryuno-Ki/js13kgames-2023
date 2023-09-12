52998cf104feb518b2cf0247e184e450fb7f9898
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-12 10:04:13 +0200

    refactor: use constants in combined reducer
    This now shares the same constants with the respective actions and
    allows for further optimisation.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/state/reducers/index.js
+++ src/js/state/reducers/index.js
@@ -1,3 +1,24 @@
+import {
+  BUY_ACTION,
+  CHECK_ON_GAMEOVER_CONDITION_ACTION,
+  CHECK_ON_WIN_CONDITION_ACTION,
+  FORWARD_TO_NEXT_MONTH_ACTION,
+  LOAD_GAME_ACTION,
+  LOAD_SHIP_ACTION,
+  RESET_ACTION,
+  SAVE_GAME_ACTION,
+  SELL_ACTION,
+  SEND_SHIP_ACTION,
+  SET_COLOR_PREFERENCE_ACTION,
+  SET_LEVEL_SCENARIO_ACTION,
+  SET_PLAYERNAME_ACTION,
+  SET_TUTORIAL_ACTION,
+  SWITCH_TO_CITY_ACTION,
+  SWITCH_TO_SCENE_ACTION,
+  SWITCH_TO_VIEW_ACTION,
+  UNLOAD_SHIP_ACTION,
+  UPDATE_SHIPS_ACTION
+} from '../../constants.js'
 import { initialState } from '../initial-state.js'
 import { buyReducer } from './buy.js'
 import { checkOnGameoverConditionReducer } from './check-on-gameover-condition.js'
@@ -32,131 +53,131 @@ export function reducer (state, action) {
 
   const { payload, type } = action
 
-  if (type === 'BUY_ACTION') {
+  if (type === BUY_ACTION) {
     return buyReducer(
       state,
       /** @type {import('../actions/buy.js').BUY_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'CHECK_ON_GAMEOVER_CONDITION_ACTION') {
+  if (type === CHECK_ON_GAMEOVER_CONDITION_ACTION) {
     return checkOnGameoverConditionReducer(
       state,
       /** @type {import('../actions/check-on-gameover-condition.js').CHECK_ON_GAMEOVER_CONDITION_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'CHECK_ON_WIN_CONDITION_ACTION') {
+  if (type === CHECK_ON_WIN_CONDITION_ACTION) {
     return checkOnWinConditionReducer(
       state,
       /** @type {import('../actions/check-on-win-condition.js').CHECK_ON_WIN_CONDITION_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'FORWARD_TO_NEXT_MONTH_ACTION') {
+  if (type === FORWARD_TO_NEXT_MONTH_ACTION) {
     return forwardToNextMonthReducer(
       state,
       /** @type {import('../actions/forward-to-next-month.js').FORWARD_TO_NEXT_MONTH_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'LOAD_GAME_ACTION') {
+  if (type === LOAD_GAME_ACTION) {
     return loadGameReducer(
       state,
       /** @type {import('../actions/load-game.js').LOAD_GAME_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'LOAD_SHIP_ACTION') {
+  if (type === LOAD_SHIP_ACTION) {
     return loadShipReducer(
       state,
       /** @type {import('../actions/load-ship.js').LOAD_SHIP_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'RESET_ACTION') {
+  if (type === RESET_ACTION) {
     return resetReducer(
       state,
       /** @type {import('../actions/reset.js').RESET_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SAVE_GAME_ACTION') {
+  if (type === SAVE_GAME_ACTION) {
     // No reducer on purpose
     return state
   }
 
-  if (type === 'SELL_ACTION') {
+  if (type === SELL_ACTION) {
     return sellReducer(
       state,
       /** @type {import('../actions/sell.js').SELL_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SEND_SHIP_ACTION') {
+  if (type === SEND_SHIP_ACTION) {
     return sendShipReducer(
       state,
       /** @type {import('../actions/send-ship.js').SEND_SHIP_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SET_COLOR_PREFERENCE_ACTION') {
+  if (type === SET_COLOR_PREFERENCE_ACTION) {
     return setColorPreferenceReducer(
       state,
       /** @type {import('../actions/set-color-preference.js').SET_COLOR_PREFERENCE_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SET_LEVEL_SCENARIO_ACTION') {
+  if (type === SET_LEVEL_SCENARIO_ACTION) {
     return setLevelScenarioReducer(
       state,
       /** @type {import('../actions/set-level-scenario.js').SET_LEVEL_SCENARIO_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SET_PLAYERNAME_ACTION') {
+  if (type === SET_PLAYERNAME_ACTION) {
     return setPlayernameReducer(
       state,
       /** @type {import('../actions/set-playername.js').SET_PLAYERNAME_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SET_TUTORIAL_ACTION') {
+  if (type === SET_TUTORIAL_ACTION) {
     return setTutorialReducer(
       state,
       /** @type {import('../actions/set-tutorial.js').SET_TUTORIAL_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SWITCH_TO_CITY_ACTION') {
+  if (type === SWITCH_TO_CITY_ACTION) {
     return switchToCityReducer(
       state,
       /** @type {import('../actions/switch-to-city.js').SWITCH_TO_CITY_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SWITCH_TO_SCENE_ACTION') {
+  if (type === SWITCH_TO_SCENE_ACTION) {
     return switchToSceneReducer(
       state,
       /** @type {import('../actions/switch-to-scene.js').SWITCH_TO_SCENE_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'SWITCH_TO_VIEW_ACTION') {
+  if (type === SWITCH_TO_VIEW_ACTION) {
     return switchToViewReducer(
       state,
       /** @type {import('../actions/switch-to-view.js').SWITCH_TO_VIEW_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'UNLOAD_SHIP_ACTION') {
+  if (type === UNLOAD_SHIP_ACTION) {
     return unloadShipReducer(
       state,
       /** @type {import('../actions/unload-ship.js').UNLOAD_SHIP_ACTION['payload']} */(payload)
     )
   }
 
-  if (type === 'UPDATE_SHIPS_ACTION') {
+  if (type === UPDATE_SHIPS_ACTION) {
     return updateShipsReducer(
       state,
       /** @type {import('../actions/update-ships.js').UPDATE_SHIPS_ACTION['payload']} */(payload)
