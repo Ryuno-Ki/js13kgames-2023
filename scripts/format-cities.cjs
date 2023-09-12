894836a79469ef4ae1da4e22eb4df6524a8bacb5
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-13 00:16:37 +0200

    chore: refine balance helper
    I figured I want to use a base ware and set the others in relationship
    to it. This way each ware is distributed roughly related to its base
    price. Then I looked after balancing out supply and demand and have each
    city provide and require about the same number of wares.
    
    No idea how well this will work but it's better than randomly guessing
    numbers.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- scripts/format-cities.cjs
+++ scripts/format-cities.cjs
@@ -1,13 +1,38 @@
-import('../src/js/state/cities.js').then((m) => console.log(m.cities.map(formatCity).join('\n')))
+Promise.all([
+	import('../src/js/state/cities.js'),
+	import('../src/js/state/wares.js')
+]).then(([cities, wares]) => {
+	console.log(formatBasePrice(wares.wares, cities.cities))
+	console.log(cities.cities.map(formatCity).join('\n\n'))
+})
+
+function formatBasePrice (wares, cities) {
+	return Object
+		.entries(wares)
+		.map((ware) => {
+			const supply = cities
+				.map((city) => city.supply[ ware[0] ])
+				.reduce((sum, summand) => sum + summand)
+			const demand = cities
+				.map((city) => city.demand[ ware[0] ])
+				.reduce((sum, summand) => sum + summand)
+
+			return `${ware[0]}: ${ware[1] / 10} (${supply} - ${demand} = ${supply - demand})`
+		})
+		.join('\t')
+}
  
 function formatWares (wares) {
 	return Object
 		.entries(wares)
 		.filter((ware) => ware[1] > 0)
 		.map((ware) => ware.join(': '))
-		.join('. ')
+		.join('\t')
 }
 
 function formatCity (city) {
-	return `${city.name}: ${formatWares(city.demand)} vs. ${formatWares(city.supply)}`
+	return [
+		`Demand of ${city.name}:\t${formatWares(city.demand)}`,
+		`Supply of ${city.name}:\t${formatWares(city.supply)}`
+	].join('\n')
 }
