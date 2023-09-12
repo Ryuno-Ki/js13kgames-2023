f3411a37d6231412821259c3e452955089d2db41
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-12 11:06:17 +0200

    test: write tests for unveilHistoryAction
    I want to make use of the historic events I tracked as state.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/state/actions/unveil-history.spec.mjs
@@ -0,0 +1,27 @@
+import chai from 'chai'
+import chaiFlux from 'chai-flux'
+import chaiInterface from 'chai-interface'
+
+import { unveilHistoryAction } from '../../../src/js/state/actions/unveil-history.js'
+
+chai.use(chaiFlux)
+chai.use(chaiInterface)
+const { expect } = chai
+
+describe('unveilHistoryAction', function () {
+  it('should create a UNVEIL_HISTORY_ACTION', function () {
+    // Arrange
+    // Nothing to prepare
+
+    // Act
+    const action = unveilHistoryAction()
+
+    // Assert
+    expect(action).to.be.an.FSA
+    expect(action).to.have.interface({
+      type: String,
+      payload: {}
+    })
+    expect(action.type).to.equal('UNVEIL_HISTORY_ACTION')
+  })
+})
