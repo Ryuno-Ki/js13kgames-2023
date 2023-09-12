f07d9fcaf91ce1414704e4941bc621c935052d3a
André Jaenisch <andre.jaenisch@posteo.de>
2023-09-12 10:03:21 +0200

    refactor: compose from smaller files
    The initial state as a whole shall remain the same. But I compose it now
    so that I can observe the bundle visualisation better.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- types/state/initial-state.d.ts
+++ types/state/initial-state.d.ts
@@ -2,69 +2,6 @@
  * @typedef {'dark' | 'light' | 'system'} Color
  */
 /**
- * @typedef {'honey' | 'salt' | 'wool'} Ware
- */
-/**
- * @typedef {object} CityDemand
- * @property {Ware} ware
- * @property {number} quantity
- */
-/**
- * @typedef {object} CitySupply
- * @property {Ware} ware
- * @property {number} quantity
- */
-/**
- * @typedef {object} WarehouseStock
- * @property {Ware} ware
- * @property {number} quantity
- */
-/**
- * @typedef {object} Warehouse
- * @property {'1' | '2' | '3' | '4'} level
- * @property {boolean} hasScribe
- * @property {Array<WarehouseStock>} stock
- */
-/**
- * @typedef {'Danzig' | 'Elbing' | 'Greifswald' | 'Hamburg' | 'Kiel' | 'Königsberg' | 'Kopenhagen' | 'Lübeck' | 'Malmö' | 'Nowgorod' | 'Reval' | 'Riga' | 'Rostock' | 'Stockholm' | 'Stralsund' | 'Turku' | 'Visby' | 'Wismar'} CityName
- */
-/**
- * @typedef {Record<CityName, number>} CityDistance
- */
-/**
- * @typedef {object} City
- * @property {CityName} name
- * @property {CityDistance} distances
- * @property {boolean} isFounded
- * @property {Array<CityDemand>} demand
- * @property {Array<CitySupply>} supply
- * @property {Warehouse} warehouse
- */
-/**
- * @typedef { '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' } Month
- */
-/**
- * @typedef {'cog' | 'nef'} ShipType
- */
-/**
- * @typedef {object} ShipTypeIntroduced
- * @property {Month} month
- * @property {number} year
- * @property {object} ship
- * @property {ShipType} ship.type
- * @property {number} ship.costs
- * @property {number} ship.maxFreightWeight
- */
-/**
- * @typedef {object} CityFounded
- * @property {Month} month
- * @property {number} year
- * @property {CityName} city
- */
-/**
- * @typedef {CityFounded | ShipTypeIntroduced} HistoryEntry
- */
-/**
  * @typedef {'about-section' | 'game-over-section' | 'level-section' | 'load-game-section' | 'new-game-section' | 'settings-section' | 'title-section' | 'win-section' | 'world-section'} Scene
  * @todo Move to scenes components
  */
@@ -76,137 +13,45 @@
  * @typedef {'free-play' | 'tutorial'} Scenario
  */
 /**
- * @typedef {object} ShipCargo
- * @property {Ware} ware
- * @property {number} quantity
- */
-/**
- * @typedef {object} ShipItinerary
- * @property {CityName} from
- * @property {CityName} to
- * @property {Month} month
- * @property {number} year
- */
-/**
- * @typedef {object} Ship
- * @property {Array<ShipCargo>} cargo
- * @property {number} costs
- * @property {ShipItinerary | null} itinerary
- * @property {number} maxFreightWeight
- * @property {boolean} moored
- * @property {string} name
- * @property {CityName | null} position
- * @property {ShipType} type
- */
-/**
- * @typedef {'docks' | 'market' | 'sea' | 'story' | 'warehouse'} View
- */
-/**
  * @typedef {object} State
- * @property {CityName} activeCity
+ * @property {import('./cities.js').CityName} activeCity
  * @property {Color} activeColor
- * @property {Month} activeMonth
+ * @property {import('./months.js').Month} activeMonth
  * @property {Scene} activeScene
  * @property {Scenario | null} activeScenario
- * @property {View} activeView
+ * @property {import('./views.js').View} activeView
  * @property {number} activeYear
- * @property {Array<City>} cities
- * @property {Array<HistoryEntry>} history
+ * @property {import('./cities.js').Cities} cities
+ * @property {import('./history.js').HistoryEntries} history
  * @property {string} playername
  * @property {number} playermoney
- * @property {Record<View, boolean>} showTutorial
- * @property {Array<Ship>} ships
- * @property {Record<Ware, number>} wares
+ * @property {import('./show-tutorial.js').ShowTutorial} showTutorial
+ * @property {import('./ships.js').Ships} ships
+ * @property {import('./wares.js').Wares} wares
  * @property {string} title
  * @property {number} volume
 */
 /** @type {State} */
 export const initialState: State;
 export type Color = 'dark' | 'light' | 'system';
-export type Ware = 'honey' | 'salt' | 'wool';
-export type CityDemand = {
-    ware: Ware;
-    quantity: number;
-};
-export type CitySupply = {
-    ware: Ware;
-    quantity: number;
-};
-export type WarehouseStock = {
-    ware: Ware;
-    quantity: number;
-};
-export type Warehouse = {
-    level: '1' | '2' | '3' | '4';
-    hasScribe: boolean;
-    stock: Array<WarehouseStock>;
-};
-export type CityName = 'Danzig' | 'Elbing' | 'Greifswald' | 'Hamburg' | 'Kiel' | 'Königsberg' | 'Kopenhagen' | 'Lübeck' | 'Malmö' | 'Nowgorod' | 'Reval' | 'Riga' | 'Rostock' | 'Stockholm' | 'Stralsund' | 'Turku' | 'Visby' | 'Wismar';
-export type CityDistance = Record<CityName, number>;
-export type City = {
-    name: CityName;
-    distances: CityDistance;
-    isFounded: boolean;
-    demand: Array<CityDemand>;
-    supply: Array<CitySupply>;
-    warehouse: Warehouse;
-};
-export type Month = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
-export type ShipType = 'cog' | 'nef';
-export type ShipTypeIntroduced = {
-    month: Month;
-    year: number;
-    ship: {
-        type: ShipType;
-        costs: number;
-        maxFreightWeight: number;
-    };
-};
-export type CityFounded = {
-    month: Month;
-    year: number;
-    city: CityName;
-};
-export type HistoryEntry = CityFounded | ShipTypeIntroduced;
 export type Scene = 'about-section' | 'game-over-section' | 'level-section' | 'load-game-section' | 'new-game-section' | 'settings-section' | 'title-section' | 'win-section' | 'world-section';
 export type SceneName = 'About' | 'GameOver' | 'Level' | 'NewGame' | 'Settings' | 'Title' | 'Win' | 'World';
 export type Scenario = 'free-play' | 'tutorial';
-export type ShipCargo = {
-    ware: Ware;
-    quantity: number;
-};
-export type ShipItinerary = {
-    from: CityName;
-    to: CityName;
-    month: Month;
-    year: number;
-};
-export type Ship = {
-    cargo: Array<ShipCargo>;
-    costs: number;
-    itinerary: ShipItinerary | null;
-    maxFreightWeight: number;
-    moored: boolean;
-    name: string;
-    position: CityName | null;
-    type: ShipType;
-};
-export type View = 'docks' | 'market' | 'sea' | 'story' | 'warehouse';
 export type State = {
-    activeCity: CityName;
+    activeCity: import('./cities.js').CityName;
     activeColor: Color;
-    activeMonth: Month;
+    activeMonth: import('./months.js').Month;
     activeScene: Scene;
     activeScenario: Scenario | null;
-    activeView: View;
+    activeView: import('./views.js').View;
     activeYear: number;
-    cities: Array<City>;
-    history: Array<HistoryEntry>;
+    cities: import('./cities.js').Cities;
+    history: import('./history.js').HistoryEntries;
     playername: string;
     playermoney: number;
-    showTutorial: Record<View, boolean>;
-    ships: Array<Ship>;
-    wares: Record<Ware, number>;
+    showTutorial: import('./show-tutorial.js').ShowTutorial;
+    ships: import('./ships.js').Ships;
+    wares: import('./wares.js').Wares;
     title: string;
     volume: number;
 };
